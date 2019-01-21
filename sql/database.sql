1e3ce9379b138fea469ff1651f4f87275d2208c3
MohamadSBarakat <mbarakat001@gmail.com>
2019-01-21 15:55:15 +0100

    Modification presque final de la BDD

--- sql/database.sql
+++ sql/database.sql
@@ -1,68 +1,65 @@
-CREATE DATABASE InstDog;
+CREATE DATABASE InstaDog;
 
 
-CREATE USER InstDog IDENTIFIED WITH mysql_native_password AS 'instdog'; 
+CREATE USER "adminInstaDog"@"localhost" IDENTIFIED BY "Inst@D0g"; 
 
 
-GRANT ALL PRIVILEGES ON *.* TO 'InstDog'@'localhost'  WITH GRANT OPTION;   
+GRANT SELECT,UPDATE,INSERT,DELETE ON InstaDog.* TO "adminInstaDog"@"localhost"  WITH GRANT OPTION;   
 
 
 
 CREATE TABLE UserDog ( 
-    id INT(100) NOT NULL  AUTO_INCREMENT,
+    id INT(100) NOT NULL AUTO_INCREMENT ,
     email varchar(255) , 
-    pasword varchar(255) ,
-    dateLastConnection DATE , 
-    nam varchar(100)  ,
-     PRIMARY KEY (id)
+    pwd varchar(255) ,
+    lastConnectionDate DATETIME ,
+    lastName varchar(100) , 
+    firstName varchar(100) ,
+    country varchar(50) ,
+    city  varchar(50) ,
+    PRIMARY KEY (id)
 ); 
 
-
--- rName varchar(100) ,
---    country varchar(50) ,
---    city varchar(50) , 
---   PRIMARY KEY (id)   
-     
---); 
 
 CREATE TABLE Dog (
     id INT(100) NOT NULL AUTO_INCREMENT ,
     nickName INT ,
     birthday DATE ,
     picture varchar(100),
-    idUser INT(100) ,
+    userId INT(100) ,
     PRIMARY KEY (id),
-    FOREIGN KEY (idUser) REFERENCES UserDog(id)
+    FOREIGN KEY (userId) REFERENCES UserDog(id)
 
 );     
 
 CREATE TABLE Article (
-    id INT(100) NOT NULL  AUTO_INCREMENT,
-    textArticle TEXT ,
-    publicationDat DATE ,
+    id INT(100) NOT NULL AUTO_INCREMENT,
+    textA TEXT ,
+    publicationDate DATETIME ,
     picture varchar(100),
-    idDog INT(100) ,
-    PRIMARY KEY (ID) ,
-    FOREIGN KEY (idDog) REFERENCES Dog(id)
+    dogId INT(100) ,
+    PRIMARY KEY (id) ,
+    FOREIGN KEY (dogId) REFERENCES Dog(id)
 
 );
 
 
 CREATE TABLE Comment (
-    id INT NOT NULL  AUTO_INCREMENT,
-    textComment varchar(250) ,
-    commentDate DATE ,
-    idArticle INT(100) ,
-    idUser INT(100) ,
+    id INT(100) NOT NULL  AUTO_INCREMENT,
+    comment Text ,
+    publicationDate DATETIME ,
+    articleId INT(100) ,
+    userId INT(100) ,
     PRIMARY KEY (id) ,
-    FOREIGN KEY (idUser) REFERENCES UserDog(id)
+    FOREIGN KEY (articleId) REFERENCES Article(id),
+    FOREIGN KEY (userId) REFERENCES UserDog(id)
     
 ); 
 
 
 CREATE TABLE Race (
     id INT(10) NOT NULL AUTO_INCREMENT,
-    name varchar(100),
+    nameRace varchar(100),
     PRIMARY KEY(id)
   
 );
