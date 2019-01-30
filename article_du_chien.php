cfbc9a6e378bda37c3d3896c3baedbef1337fbe8
Anjuide <anjuide@users.noreply.github.com>
2019-01-30 11:53:09 +0100

    Modification

--- article_du_chien.php
+++ article_du_chien.php
@@ -3,179 +3,107 @@ $title = "Profil du chien";
 include "header.php"
 ?>
 <div class="post post-single" style="background-image: url(http://usercontent.packdog.com/post/photo/bg/Ye9f19_3c697b22ae9363b1901b0d65fb46447d6e698659.jpg); background-color: #B5B5B5;">
-
-	<div class="post-bgscreen">
-		
+	<div class="post-bgscreen">	
 		<div class="post-container">
-
 			<div class="post-header">
-				
-				<h2><a href="/chester-65" class="post-dogname" title="See more Marvelous Mystery Mutt photos of Chester">Indoor</a> la <a href="http://packdog.com/breed/marvelous-mystery-mutt" class="post-breedname" title="Marvelous Mystery Mutt photos">petite rapace</a> par <a href="/human/todd-55" class="post-humanname">Ange Gnamba</a></h2>		
-
+				<h2>
+					<a href="/chester-65" class="post-dogname" title="See more Marvelous Mystery Mutt photos of Chester">Rod</a> la 
+					<a href="http://packdog.com/breed/marvelous-mystery-mutt" class="post-breedname" title="Marvelous Mystery Mutt photos">petite rapace</a> par 
+					<a href="/human/todd-55" class="post-humanname">Ange Gnamba</a>
+				</h2>		
 				<div class="post-stamp">
-					<div class="post-time">46w</div>
-					
+					<div class="post-time">25.01.1989</div>
 				</div>
 			</div><!-- post-header -->
 
 			<div class="post-content">
 				<img src="http://usercontent.packdog.com/post/photo/Ye9f19_3c697b22ae9363b1901b0d65fb46447d6e698659.jpg" alt="here we go again - Chester the Marvelous Mystery Mutt">
 			</div>
-
 			<div class="post-actions">
-		
 				<div class="post-caption">
 					<p>
-			
-							Another huge storm is arriving late tonight, oh spring where are you
-
+						Une autre journée de neige au RDV...........
 					</p>
 				</div>
-				
 				<ul class="post-actiontab">
 
 				</ul>
 			</div><!-- post-actions -->			
 
 			<div class="post-comments" id="comments">
-				<h3><span class="post-commentstitle iconpostcomments"><span data-bind="comment-count">8</span> comments</span></h3>
+				<h3>
+					<span class="post-commentstitle iconpostcomments">
+						<span data-bind="comment-count">8</span> commentaires
+					</span>
+				</h3>
 				<ul class="commentlist">
-					
-					
-	
-	<li class="comment" id="comment_189914" data-id="189914">
-
-
-	<div class="comment-author">
-		<strong><a href="http://packdog.com/human/molly-83">Molly Bruce</a></strong>
-	</div>
-  
-	<p>What a cute tongue!</p>
-  
-	<div class="comment-time">44w</div>
-
-</li><!-- comment -->
-	
-
-	
-	<li class="comment" id="comment_189767" data-id="189767">
-
-
-	<div class="comment-author">
-		<strong><a href="http://packdog.com/human/paulie">Paulie Goforth</a></strong>
-		
-		(and <a href="/wally-48">Wally</a>)
-		
-	</div>
-  
-	<p>Yay!  Congrats on being Editor’s Pick!</p>
-  
-	<div class="comment-time">44w</div>
-
-</li><!-- comment -->
-	
-
-	
-	<li class="comment" id="comment_188916" data-id="188916">
-
-
-	<div class="comment-author">
-		<strong><a href="http://packdog.com/human/hollychristine">Holly Christine</a></strong>
-		
-		(and <a href="/chewy-56">Chewy</a> and <a href="/shorty-22">Shorty</a>)
-		
-	</div>
-  
-	<p>Me and the pups send best wishes and safety to you and your family!</p>
-  
-	<div class="comment-time">46w</div>
-
-</li><!-- comment -->
-	
-
-	
-	<li class="comment" id="comment_188905" data-id="188905">
-
-
-	<div class="comment-author">
-		<strong><a href="http://packdog.com/human/tinna">Tinna K</a></strong>
-		
-	</div>
-  
-	<p>Hold on tight 🐾❤️</p>
-  
-	<div class="comment-time">46w</div>
-
-</li><!-- comment -->
-	
-
-	
-	<li class="comment" id="comment_188900" data-id="188900">
-
-
-	<div class="comment-author">
-		<strong><a href="http://packdog.com/human/paulie">Paulie Goforth</a></strong>
-		
-	</div>
-  
-	<p>Stay safe and hope you don’t lose power!</p>
-  
-	<div class="comment-time">46w</div>
-
-</li><!-- comment -->
-	
-
-	
-	<li class="comment" id="comment_188889" data-id="188889">
-
-
-	<div class="comment-author">
-		<strong><a href="http://packdog.com/human/mieke">Mieke T.</a></strong>
-		
-		
-	</div>
-  
-	<p>Oh..I wish I could send you our weather..it 's soft here.. The wind looks really cold! Here 's a saying: 'March moves it 's tail, April does what he wants, in May all cuckoos lay their egg!'. About March they are right at your side! Go under the blankets Chester and stay warm. We will cross our fingers that the power will stay on! ♡♡♡</p>
-  
-	<div class="comment-time">46w</div>
-
-</li><!-- comment -->
-	
-
-	
-	<li class="comment" id="comment_188875" data-id="188875">
-
-
-	<div class="comment-author">
-		<strong><a href="http://packdog.com/human/patricia-72">Patricia Davis</a></strong>
-	
-		
-	</div>
-  
-	<p>Maybe stolen by then there Ides of March..😉😆❄❄❄❄🐾🐾🐾🐾</p>
-  
-	<div class="comment-time">46w</div>
-
-</li><!-- comment -->
-	
-
-	
-	<li class="comment" id="comment_188868" data-id="188868">
-
-
-	<div class="comment-author">
-		<strong><a href="http://packdog.com/human/alicewalter">Alice Walter</a></strong>
-		
-		
-	</div>
-  
-	<p>Be strong!</p>
-  
-	<div class="comment-time">46w</div>
-
+					<li class="comment" id="comment_189914" data-id="189914">
+						<div class="comment-author">
+							<strong><a href="#">John Doe</a></strong>
+						</div>
+						<p>Quel beau chien!</p>
 				
-			</div><!-- post-comments -->
-			
+						<div class="comment-time">25.01.1989</div>
+					</li><!-- comment -->
+					
+					<li class="comment" id="comment_189767" data-id="189767">
+						<div class="comment-author">
+							<strong><a href="#">Sandra Realise</a></strong>
+						</div>
+						<p>En mode pensif!</p>
+						<div class="comment-time">25.01.1989</div>
+					</li><!-- comment -->
+					
+					<li class="comment" id="comment_188916" data-id="188916">
+						<div class="comment-author">
+							<strong><a href="#">Vincent Realise</a></strong>	
+						</div>
+						<p>Big dog</p>
+						<div class="comment-time">25.01.1989</div>
+					</li><!-- comment -->
+
+					<li class="comment" id="comment_188905" data-id="188905">
+						<div class="comment-author">
+							<strong><a href="#">Ange Gnamba</a></strong>
+						</div>
+						<p>Bon week-end à tous 🐾❤️</p>
+						<div class="comment-time">25.01.1989</div>
+					</li><!-- comment -->
+					
+					<li class="comment" id="comment_188900" data-id="188900">
+						<div class="comment-author">
+							<strong><a href="#">John Doe</a></strong>
+						</div>
+						<p>Merci pareil à toi aussi! Garde la forme</p>
+						<div class="comment-time">25.01.1989</div>
+					</li><!-- comment -->
+				
+					<li class="comment" id="comment_188889" data-id="188889">
+						<div class="comment-author">
+							<strong><a href="#">Ange Gnamba</a></strong>
+						</div>
+						<p>Ah j'avais oublié de mentionner que je ne sais pas quoi dire encore. Je dois faire un commentaire qui est assez long pour voir une différence entre les commentaires courts et les commentaires longs comme celui que je suis en train de faire! Je pense qu'il est pas assez long donc je vais continuer d'écrire des petites choses qui veulent pas forcement dire quelques choses. Ohh regardez! Des Coeurs! Oh que c'est joli! ♡♡♡ Bonne journée</p>
+						<div class="comment-time">25.01.1989</div>
+					</li><!-- comment -->
+
+					<li class="comment" id="comment_188875" data-id="188875">
+						<div class="comment-author">
+							<strong><a href="#">Sandra Realise</a></strong>
+						</div>				
+						<p>Moi aussi je sais faire des smileys ..😉😆❄❄❄❄🐾🐾🐾🐾</p>	
+						<div class="comment-time">25.01.1989</div>
+					</li><!-- comment -->
+						
+					<li class="comment" id="comment_188868" data-id="188868">
+						<div class="comment-author">
+							<strong><a href="#">Vincent Realise</a></strong>
+						</div>
+						<p>Bravo!</p>
+						<div class="comment-time">25.01.1989</div>
+					</li><!-- comment -->
+
+				</ul>	<!-- comment list -->			
+			</div><!-- post-comments -->		
 
 		</div><!-- post-container -->
 	</div><!-- post-bgscreen -->
