aeabe0014c5bb647cf60255acec445949a60e28e
Anjuide <ange.gnamba@gmail.com>
2019-02-03 22:03:22 +0100

    Centrage des images et ajout commentaires

--- index.php
+++ index.php
@@ -1,193 +1,195 @@
 <?php 
+// Titre de la page
 $title = "Bienvenue sur Instadog";
+// INCLURE LE HEADER
 include "header.php"
 ?>
+<!-- CONTAINER DU BODY -->
 <div class="container">
-<div class="row">
-
-
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D1.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>     
+    <div class="row">
+        <!-- IMAGE DU CHIEN ET SON NICKNAME -->
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D1.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>     
+                </div>
+            </a>
         </div>
-        </a>
 
-    </div>
-
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D2.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>   
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D2.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>   
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D3.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4> 
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D3.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4> 
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D4.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>  
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D4.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>  
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D5.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4> 
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D5.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4> 
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D6.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D6.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D7.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D7.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D8.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D8.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D9.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D9.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D10.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>  
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D10.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>  
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D18.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4> 
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D18.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4> 
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D12.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4> 
-        </a>
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D12.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4> 
+                </a>
+            </div>
         </div>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D13.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D13.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D14.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>  
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D14.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>  
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D15.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4> 
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D15.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4> 
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D16.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>   
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D16.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>   
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D17.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>   
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D17.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>   
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D18.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>  
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D18.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>  
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D19.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>  
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D19.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>  
+                </div>
+            </a>
         </div>
-        </a>
-    </div>
 
-    <div class="card" style="width: 17rem; col-lg-3 col-md-4">
-        <a href="profil_du_chien.php">
-        <img class="card-img-top" src="images/D17.jpeg" alt="Card image">
-        <div class="card-img-overlay">
-        <h4 class="card-title dogyy">Scopy Dooo</h4>  
+        <div class="card col-lg-3 col-md-4" style="width: 17rem; margin:auto; padding:0;">
+            <a href="profil_du_chien.php">
+                <img class="card-img-top" src="images/D17.jpeg" alt="Photo de profil de ">
+                <div class="card-img-overlay">
+                    <h4 class="card-title dogyy">Scooby-Dooo</h4>  
+                </div>
+            </a>
         </div>
-        </a>
     </div>
-</div>
 </div>
 <?php 
+// INCLURE FOOTER
 include "footer.php"
 ?>
