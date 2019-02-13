28ac4758e0217888f4f2f57f9e51f82efadc6bda
Anjuide <ange.gnamba@gmail.com>
2019-02-13 23:34:48 +0100

    Mot de passe dur

--- inscription.php
+++ inscription.php
@@ -1,143 +1,76 @@
 <?php 
-$title = "Profil de l'utilisateur";
+// SE CONNECTER A LA BASE DE DONNEES
+require("classes/Connexion.php");
+// ON DEMARRE LA SESSION AVANT D'ECRIRE DU CODE HTML
+session_start();
+// DEMARRER UN NOUVEL OBJET DE CONNEXION
+$appli = new Connexion();
+// TITRE DE LA PAGE
+$title = "Profil utilisateur";
+// INCLURE LE HEADER
 include "header.php";
-require('config.php');
-//include ('login.php');
-//$appli  = new Connexion();
 
 ?>
 <div class="container">
-    <div class="row">
-      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div class="card card-signin my-5">
-          <div class="card-body">
-            <h5 class="card-title text-center">Inscrivez-vous</h5>
-            <hr class="my-4">
-            <form method="post" action="login.php" class="form-signin">
-
-              <div class="form-label-group">
-                <label for="inputEmail">Adresse e-mail</label>
-                <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Adresse e-mail" required autofocus> 
-              </div>
-
-              <div class="form-label-group">
-                <label for="inputPassword">Mot de passe</label>
-                <input type="password" name="password_1" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
-              </div>
-
-              <div class="form-label-group">
-                <label for="inputPasswordConfirm">Confirmez le mot de passe</label>
-                <input type="password" name="password_2" id="inputPasswordConfirm" class="form-control" placeholder="Confirmez le mot de passe" required>
-              </div>
-              <hr class="my-4">
-              
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" name="reg_user" type="submit">S'inscrire</button>
-                <div class="text-center">
-                  <a class="small" href="connexion.php">Vous avez déjà un compte? Connectez-vous ici</a>
+  <div class="row">
+    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+      <div class="card card-signin my-5">
+        <div class="card-body">
+          <h5 class="card-title text-center">Inscrivez-vous</h5>
+          <hr class="my-4">
+          <form class="form-signin needs-validation" action="" method="post" enctype="multipart/form-data" novalidate >
+            <?php include "inscription-form.php";?>
+            <div class="form-label-group">
+              <label for="inputEmail">Adresse e-mail</label>
+              <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Adresse e-mail" required autofocus> 
+              <div class="invalid-feedback">
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                  <strong>Email!</strong> non valide.
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
                 </div>
-              <!-- <hr class="my-4">
-              <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Continuer avec Google</button>
-              <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Continuer avec Facebook</button> -->
-            </form>
-          </div>
+              </div>
+            </div>
+
+            <div class="form-label-group">
+              <label for="inputPassword">Mot de passe (au moins 6 caractères)</label>
+              <input type="password" id="inputPassword" name="password" minlength=6 class="form-control" pattern="(?=.*\d)(?=.*[A-Z])(?=.*[~`!@#$%^&*()\-_+={};:\[\]\?\.\\/,]).{6,}" title="Le mot de passe doit contenir une lettre majuscule, un caractère spécial et un chiffre." placeholder="Mot de passe" required onchange="checkPassword()">
+              <div class="invalid-feedback">
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                  <strong>Mot de passe!</strong> non valide.
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+              </div>
+              <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <small>Le mot de passe doit contenir une lettre majuscule, un caractère spécial et un chiffre.</small> 
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>  
+              </div>
+            </div>
+
+            <div class="form-label-group">
+              <label for="inputPasswordConfirm">Confirmez le mot de passe</label>
+              <input type="password" id="inputPasswordConfirm" name="password-repeat" minlength=5 class="form-control" pattern="(?=.*\d)(?=.*[A-Z])(?=.*[~`!@#$%^&*()\-_+={};:\[\]\?\.\\/,]).{6,}" title="Veuillez saisir le même mot de passe" placeholder="Confirmez le mot de passe" required onkeyup="checkPassword()">
+            </div>
+            <hr class="my-4">
+            
+            <button class="btn btn-lg btn-success btn-block text-uppercase" name="inscription" type="submit">S'inscrire</button>
+              <div class="text-center">
+                <span class="small">Vous avez déjà un compte? </span>
+                <a class="small" href="connexion.php">Connectez-vous ici</a>
+              </div>
+          </form>
+          
         </div>
       </div>
     </div>
   </div>
-
-  <?php
-
-$appli       = new Connexion();
-
-session_start();
-
-// initializing variables
-
-
-$email    = "";
-
-$errors = array(); 
-
-if (isset($_POST['reg_user'])) {
-
-  // receive all input values from the form
-
-   
-  $email =  ($_POST['email']);
-
-  $password_1 =  ($_POST['password_1']);
-
-  $password_2 =  ($_POST['password_2']);
-
-
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
-
-
-  if (empty($email)) { array_push($errors, "Email is required"); }
-
-  if (empty($password_1)) { array_push($errors, "Password is required"); }
-
-  if ($password_1 != $password_2) {array_push($errors, "The two passwords do not match");}
-
-  // first check the database to make sure 
-  // a user does not already exist with the same username and/or email
-
-  $user = $appli->checkUserIsExist($email);
-
-  if ($user) { // if user exists
-     
-      array_push($errors, "email already exists");
-      
-      
-      var_dump($user);
-
-
-  }
-  // Finally, register user if there are no errors in the form
-  
-  if (count($errors) == 0) {
-
-  	$pwd = password_hash($password_1, PASSWORD_DEFAULT); //encrypt the password before saving in the database
-    $appli->insertUser($email, $pwd);
-
-    $id = $appli->getLastId();
-
-    $appli->insertLastConnectionDateToUser($id);
-
-    $_SESSION['username'] = $email;
-
-    $_SESSION['userId'] = $id;
-
-    $_SESSION['success'] = "You are now logged in";
-
-    echo 'You are now logged in';
-
-    echo "</br>" . "</br>";
-
-    echo $id;
-    
-  //echo "<script> window.location.href =\"inscription.php\"</script>";
-
-  } else {
-
-    echo 'there are  errors';
-
-   // echo "<script> window.location.href =\"inscription.php\"</script>";
-
-  }
-
-
-
-
-
-
-}
-
-  ?>
-
-
-
+</div>
+<!-- INCLURE LE FOOTER -->
 <?php 
 include "footer.php"
 ?>
