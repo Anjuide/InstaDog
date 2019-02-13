b1c468aa9b7210f86efe1c957aca0e3a12058647
Anjuide <ange.gnamba@gmail.com>
2019-02-13 23:33:15 +0100

    session start et commentaires

--- connexion.php
+++ connexion.php
@@ -1,105 +1,108 @@
 <?php 
-$title = "Profil de l'utilisateur";
+// SE CONNECTER A LA BASE DE DONNEES
+require("classes/Connexion.php");
+// ON DEMARRE LA SESSION AVANT D'ECRIRE DU CODE HTML
 session_start();
-include "header.php";
-require('config.php');
+// DEMARRER UN NOUVEL OBJET DE CONNEXION
 $appli = new Connexion();
-
+// TITRE DE LA PAGE
+$title = "Bienvenue sur Instadog";
+// INCLURE LE HEADER
+include "header.php";
 
 ?>
 <div class="container">
-    <div class="row">
-      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div class="card card-signin my-5">
-          <div class="card-body">
-            <h5 class="card-title text-center">Connexion</h5>
-            <hr class="my-4">
-            <form method="post" action="connexion.php" class="form-signin">
-              <div class="form-label-group">
-                <label for="inputEmail">Adresse e-mail</label>
-                <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Adresse e-mail" required autofocus> 
-              </div>
+  <div class="row">
+    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+      <div class="card card-signin my-5">
+        <div class="card-body">
+          <h5 class="card-title text-center">Connexion</h5>
+          <hr class="my-4">
+          <form class="form-signin" action="" method="post" enctype="multipart/form-data" >
+            <?php include "connexion-form.php";?>
+            <div class="form-label-group">
+              <label for="inputEmail">Adresse e-mail</label>
+              <input type="email" id="inputEmail"  name="email" class="form-control" placeholder="Adresse e-mail" required autofocus> 
+            </div>
 
-              <div class="form-label-group">
-                <label for="inputPassword">Mot de passe</label>
-                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+            <div class="form-label-group">
+              <label for="inputPassword">Mot de passe</label>
+              <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Mot de passe" required>
+            </div>
+            <hr class="my-4">
+            <button class="btn btn-lg btn-success btn-block text-uppercase" name="connexion" type="submit">Connexion</button>
+            <div class="text-center">
+                <a class="small" href="#">Mot de passe oublié?</a>
+            </div>
+            <div class="text-center">
+              <span class="small">Vous n'avez pas de compte? </span>
+                <a class="small" href="inscription.php">Inscrivez-vous ici</a>
               </div>
-              <hr class="my-4">
-               
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" name="log_user" type="submit">Connexion</button>
-              <div class="text-center">
-                  <a class="small" href="#">Mot de passe oublié?</a>
-              </div>
-              <div class="text-center">
-                 <a class="small" href="inscription.php">Vous n'avez pas de compte? Inscrivez-vous ici</a>
-               </div>
-              <!-- <hr class="my-4">
-              <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Continuer avec Google</button>
-              <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Continuer avec Facebook</button> -->
-            </form>
-          </div>
+          </form>
         </div>
       </div>
     </div>
   </div>
+</div>
 
 <?php
-  $email    = "";
-  $errors = array(); 
+//   $email    = "";
+//   $errors = array(); 
   
-  if (isset($_POST['log_user'])) {
+//   if (isset($_POST['connexion'])) {
 
-  // receive all input values from the form
+//   // receive all input values from the form
 
-    $email    =  ($_POST['email']);
-    $password =  ($_POST['password']);
+//     $email    =  ($_POST['email']);
+//     $password =  ($_POST['password']);
 
-    // form validation: ensure that the form is correctly filled ...
-    // by adding (array_push()) corresponding error unto $errors array
+//     // form validation: ensure that the form is correctly filled ...
+//     // by adding (array_push()) corresponding error unto $errors array
 
-  if (empty($email)) { array_push($errors, "Email is required"); }
+//   if (empty($email)) { array_push($errors, "Email is required"); }
 
-  if (empty($password)) { array_push($errors, "Password is required"); }
+//   if (empty($password)) { array_push($errors, "Password is required"); }
 
-  // first check the database to make sure 
-  // a user is already exist with the same  email
+//   // first check the database to make sure 
+//   // a user is already exist with the same  email
 
-  $user = $appli->checkUserIsExist($email);
+//   $user = $appli->checkUserIsExist($email);
 
-  if ($user) { // if user exists
+//   if ($user) { // if user exists
      
-    $pwd = $user->getPwd();
-    $id  = $user->getId();
+//     $pwd = $user->getPwd();
+//     $id  = $user->getId();
     
-    if (password_verify($password, $pwd)) { //Verify the Entered password With saving in the database
+//     if (password_verify($password, $pwd)) { //Verify the Entered password With saving in the database
 
-      $appli->insertLastConnectionDateToUser($id);
+//       $appli->insertLastConnectionDateToUser($id);
 
-      $_SESSION['username'] = $email;
-      $_SESSION['userId'] = $id;
-      $_SESSION['success'] = "You are now logged in";
+//       $_SESSION['username'] = $email;
+//       $_SESSION['userId'] = $id;
+//       $_SESSION['success'] = "You are now logged in";
 
-      echo 'You are now logged in';
+//       echo 'You are now logged in';
 
-      echo $id;
+//       echo $id;
 
-      echo "<script> window.location.href =\"profileAvecLeChien.php\"</script>";
+//       echo "<script> window.location.href =\"profileAvecLeChien.php\"</script>";
 
 
-    } else {
+//     } else {
 
-      echo 'Mouvais un mot pass ou un email  ';
+//       echo 'Mouvais un mot pass ou un email  ';
 
-    }  
+//     }  
  
      
-  }
+//   }
     
 
-}
+// }
 ?>
 
 
 <?php 
+
 include "footer.php"
 ?>
