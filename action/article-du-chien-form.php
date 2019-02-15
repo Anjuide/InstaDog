29b97dfa6328ad1bab4426087f7f6ec6aea556a4
Anjuide <anjuide@users.noreply.github.com>
2019-02-15 11:16:29 +0100

    Repare redirection

--- action/article-du-chien-form.php
+++ action/article-du-chien-form.php
@@ -10,7 +10,7 @@ if (isset($_POST['ajouterCommentaire'])) {
         $publicationDate = date('Y-m-d H:i:s');
         // APPEL DE MA FONCTION QUI AJOUTE UN ARTICLE
         $appli->setComment($comment, $publicationDate, $articleId, $userId);
-       echo "<script type='text/javascript'>document.location.replace('article-du-chien?id=$articleId.php#comments');</script>";
+       echo "<script type='text/javascript'>document.location.replace('article-du-chien.php?id=$articleId');</script>";
     } else {
         echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
                 Veuillez remplir ce champ obligatoires.
